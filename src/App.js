25525067b01d198bab5188575c64094308066052
oriofir <97814056+oriofir@users.noreply.github.com>
2023-05-17 17:17:46 -0700

    csv converting to JSON

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import React, { useState } from "react";
+import DragAndDrop from "./Components/DragAndDrop";
 import "./App.css";
 
-const DragAndDropCSVApp = () => {
-  const [csvData, setCSVData] = useState(null);
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-
-    const file = e.dataTransfer.files[0];
-    const reader = new FileReader();
-
-    reader.onload = () => {
-      const data = reader.result;
-      setCSVData(data);
-    };
-
-    reader.readAsText(file);
-  };
-
+function App() {
   return (
-    <div className="dragBox">
-      <div
-        className="dottedBox"
-        onDrop={handleDrop}
-        onDragOver={(e) => e.preventDefault()}
-        style={{}}
-      >
-        Drop CSV file here
-      </div>
-
-      {csvData && (
-        <div>
-          <h3>CSV Data:</h3>
-          <pre>{csvData}</pre>
-        </div>
-      )}
+    <div className="App">
+      <DragAndDrop />
     </div>
   );
-};
+}
 
-export default DragAndDropCSVApp;
+export default App;
