f1deb223b3a06d7cb65ddb6ca22bf800aec7b27f
oriofir <97814056+oriofir@users.noreply.github.com>
2023-05-23 10:40:45 -0700

    mapped out csv coming out properly as JSON

--- src/Components/DragAndDrop.js
+++ src/Components/DragAndDrop.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
 
-const dragAndDrop = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+const DragAndDrop = () => {
   const [jsonData, setJsonData] = useState(null);
 
   const handleDrop = (e) => {
@@ -13,77 +12,56 @@ const dragAndDrop = () => {
 
     reader.onload = () => {
       const csvData = reader.result;
-      const jsonData = convertCSVtoJSON(csvData);
+      const { data } = Papa.parse(csvData);
+
+      if (!data.length) {
+        // Handle empty CSV file
+        return "No data found in the CSV file.";
+      }
+
+      const template = {
+        PO_NUMBER: null,
+        FREIGHT_TRACKING: null,
+        SO_NUMBER: null,
+        FIRST_NAME: null,
+        LAST_NAME: null,
+        ADDRESS_1: null,
+        ADDRESS_2: null,
+        CITY: null,
+        STATE: null,
+        ZIP: null,
+        PHONE1: null,
+        PHONE2: null,
+        EMAIL: null,
+        ITEM_SKU: null,
+        // ... continue with the rest of the template keys
+      };
+
+      const headers = data[0];
+      const mappedData = data.slice(1).map((row) => {
+        const mappedRow = { ...template };
+
+        for (let i = 0; i < row.length; i++) {
+          const cellValue = row[i];
+          const header = headers[i];
+
+          for (const key in mappedRow) {
+            const regex = new RegExp(key, "i");
+            if (regex.test(header)) {
+              mappedRow[key] = cellValue;
+              break;
+            }
+          }
+        }
+
+        return mappedRow;
+      });
+
+      const jsonData = JSON.stringify(mappedData, null, 2);
       setJsonData(jsonData);
     };
 
     reader.readAsText(file);
-  };
-
-  const convertCSVtoJSON = (csvData) => {
-    const { data, meta } = Papa.parse(csvData, { header: true });
-
-    if (!data.length) {
-      // Handle empty CSV file
-      return "No data found in the CSV file.";
-    }
-
-    const templateHeaders = [
-      "PO_NUMBER",
-      "FREIGHT_TRACKING",
-      "SO_NUMBER",
-      "FIRST_NAME",
-      "LAST_NAME",
-      "ADDRESS1",
-      "ADDRESS2",
-      "CITY",
-      "STATE",
-      "ZIP",
-      "PHONE1",
-      "PHONE2",
-      "EMAIL",
-      "ITEM_SKU",
-      "ITEM_SERIAL_NUMBER",
-      "ITEM_DESCRIPTION",
-      "ITEM_CATEGORY",
-      "WEIGHT",
-      "CUBE",
-      "LENGTH",
-      "WIDTH",
-      "HEIGHT",
-      "COMMODITY_TYPE",
-      "RETAIL_VALUE",
-      "INSURANCE_COVERAGE_AMOUNT",
-      "QUANTITY",
-      "LINKS",
-      "VENDOR",
-      "NOTES",
-      "DRIVER_NOTES",
-      "SERVICE_LEVEL",
-      "PICKUP_ADDRESS1",
-      "PICKUP_ADDRESS2",
-      "PICKUP_CITY",
-      "PICKUP_STATE",
-      "PICKUP_ZIP",
-      "PICKUP_COMPANY",
-      "PICKUP_CONTACT_NAME",
-      "PICKUP_CONTACT_PHONE",
-      "PICKUP_CONTACT_EMAIL",
-      "STOP_TYPE",
-      "MASK_CUSTOMER_INFORMATION",
-    ];
-
-    const mappedData = data.map((row) => {
-      const mappedRow = {};
-
-      templateHeaders.forEach((header) => {
-        mappedRow[header] = row[header] || "";
-      });
-
-      return mappedRow;
-    });
-
-    return JSON.stringify(mappedData, null, 2);
   };
 
   return (
@@ -106,4 +84,4 @@ const dragAndDrop = () => {
   );
 };
 
-export default dragAndDrop;
+export default DragAndDrop;
