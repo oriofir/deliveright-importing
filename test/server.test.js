96d0f8057a9f0c13caa3714dd3c9c19a23919478
oriofir <97814056+oriofir@users.noreply.github.com>
2023-06-15 14:40:38 -0700

    all unit tests pass

---
+++ test/server.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+const app = require("./server");
+
+describe("Server", () => {
+  test("GET / should return status code 200", async () => {
+    const response = await request(app).get("*");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("POST /upload-csv should return status code 200", async () => {
+    const response = await request(app).post("/upload-csv");
+    expect(response.statusCode).toBe(200);
+  });
+});
